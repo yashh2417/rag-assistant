1de3c08638a8bc68a7fa89873556b23385ce366d
Yash <yashh2417@gmail.com>
2025-06-07 16:33:08 +0530

    structure updated

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 from setuptools import setup, find_packages
-  
-setup( 
-    name='mcqgenerator', 
-    version='0.0.1', 
-    author='Yash', 
-    author_email='yashh2417@gmail.com', 
-    install_requires=[ 
-        "openai",
-        "langchain",
-        "streamlit",
-        "python-dotenv",
-        "PyPDF2",
-        "langchain-deepseek"
-    ], 
-    packages=find_packages() 
-) 
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    description='generate mcq on the basis of context given.',
+    author='Yash',
+    author_email='yashh2417@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'streamlit',
+        'google-generativeai',
+        'python-dotenv',
+        'langchain',
+        'PyPDF2',
+        'chromadb',
+        'faiss-cpu',
+        'langchain_google_genai',
+        'fastapi',
+        'pydantic'
+    ],
+    python_requires='>=3.8',
+)
+
+-e .
